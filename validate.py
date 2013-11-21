7c9be04f0491c4a34af5e881a94a4e1fd2fbf867
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-11-22 00:49:31 +0400

    Validate coursework number

--- validate.py
+++ validate.py
@@ -3,6 +3,14 @@
 import os
 
 curr_dir = os.environ['TRAVIS_BUILD_DIR']
-username = os.path.dirname(curr_dir)
+username = os.path.basename(os.path.dirname(curr_dir))
 
-print username
+dirs = ['1_poll', '2_quiz', '3_gallery']
+
+workdir = dirs[ord(username[0])]
+
+for dirname in dirs:
+	if dirname == workdir:
+		return
+	if len(os.listdir(os.path.join(curr_dir, dirname))) > 1:
+		raise Error("Not yours work")
