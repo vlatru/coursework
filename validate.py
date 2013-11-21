7870a4498f2fc66f3d33561e3954f1a5b376ba2f
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-11-22 01:02:11 +0400

    Tabs to spaces

--- validate.py
+++ validate.py
@@ -10,7 +10,7 @@ dirs = ['1_poll', '2_quiz', '3_gallery']
 workdir = dirs[ord(username[0])]
 
 for dirname in dirs:
-	if dirname == workdir:
-		return
-	if len(os.listdir(os.path.join(curr_dir, dirname))) > 1:
-		raise Error("Not yours work")
+    if dirname == workdir:
+        return
+    if len(os.listdir(os.path.join(curr_dir, dirname))) > 1:
+        raise Error("Not yours work")
