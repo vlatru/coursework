fd810b91eed8c02bf36c1e2bdb31bd14cbcf5405
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-11-23 13:14:09 +0400

    Добавил проверку правильности выбора тестового задания

---
+++ validate.py
@@ -0,0 +1,26 @@
+# encoding=utf-8
+
+import os
+import sys
+import json
+import urllib2
+
+repo = os.environ['TRAVIS_REPO_SLUG']
+pull_number = os.environ['TRAVIS_PULL_REQUEST']
+api_head = "https://api.github.com/repos/"
+api_request_url = "{}{}/pulls/{}".format(api_head, repo, pull_number)
+
+response = urllib2.urlopen(api_request_url).read()
+pull_data = json.loads(response)
+username = pull_data['user']['login']
+
+dirs = ['1_poll', '2_quiz', '3_gallery']
+
+workdir = dirs[ord(username[0]) % 3]
+
+for dirname in dirs:
+    if dirname == workdir:
+        break
+    if len(os.listdir(dirname)) > 1:
+        print("Ошибка: \"{}\" не ваше задание".format(dirname))
+        sys.exit(1)
