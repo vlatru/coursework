1ef1e77c31bc84302a7d5cb76b0c05bc4249b94e
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-11-23 13:27:11 +0400

    Пропускаем валидацию на не пулл-реквестах

--- validate.py
+++ validate.py
@@ -10,17 +10,21 @@ pull_number = os.environ['TRAVIS_PULL_REQUEST']
 api_head = "https://api.github.com/repos/"
 api_request_url = "{}{}/pulls/{}".format(api_head, repo, pull_number)
 
-response = urllib2.urlopen(api_request_url).read()
-pull_data = json.loads(response)
-username = pull_data['user']['login']
+try:
+    response = urllib2.urlopen(api_request_url).read()
+except urllib2.HTTPError:
+    pass
+else:
+    pull_data = json.loads(response)
+    username = pull_data['user']['login']
 
-dirs = ['1_poll', '2_quiz', '3_gallery']
+    dirs = ['1_poll', '2_quiz', '3_gallery']
 
-workdir = dirs[ord(username[0]) % 3]
+    workdir = dirs[ord(username[0]) % 3]
 
-for dirname in dirs:
-    if dirname == workdir:
-        break
-    if len(os.listdir(dirname)) > 1:
-        print("Ошибка: \"{}\" не ваше задание".format(dirname))
-        sys.exit(1)
+    for dirname in dirs:
+        if dirname == workdir:
+            break
+        if len(os.listdir(dirname)) > 1:
+            print("Ошибка: \"{}\"	 не ваше задание".format(dirname))
+            sys.exit(1)
